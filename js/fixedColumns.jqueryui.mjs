2c496abc57d23facd877ca101bc1894d0c9f35e1
Allan Jardine <allan@datatables.net>
2022-10-10 19:49:14 +0000

    Add support for mjs files (ES Modules)

---
+++ js/fixedColumns.jqueryui.mjs
@@ -0,0 +1,13 @@
+
+/*! jquery ui integration for DataTables' FixedColumns
+ * © SpryMedia Ltd - datatables.net/license
+ */
+
+import $ from 'jquery';
+import DataTable from 'datatables.net-ju';
+import DataTable from 'datatables.net-fixedcolumns';
+
+
+
+
+export default DataTable;
