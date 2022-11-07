aa57937113e3bc366b70d9bf3406b62ad78a6086
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-07 16:12:53 +0000

    7bc6dcd5053b2d6128798325bbdf202dc14ef38c Release 4.2.1 (packaging error with 4.2.0)
    Sync to source repo @7bc6dcd5053b2d6128798325bbdf202dc14ef38c

--- js/fixedColumns.jqueryui.mjs
+++ js/fixedColumns.jqueryui.mjs
@@ -1,11 +1,10 @@
-
 /*! jquery ui integration for DataTables' FixedColumns
  * © SpryMedia Ltd - datatables.net/license
  */
 
 import $ from 'jquery';
 import DataTable from 'datatables.net-ju';
-import DataTable from 'datatables.net-fixedcolumns';
+import 'datatables.net-fixedcolumns';
 
 
 
